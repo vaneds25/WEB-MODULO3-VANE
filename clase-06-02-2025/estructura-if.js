ff52bccf7deb57249f860b632d859486fe70f44a
Vane <vaneduransierra@gmail.com>
2025-03-02 16:49:10 -0600

    Agrego ejercicio OPCIONAL

--- clase-06-02-2025/estructura-if.js
+++ clase-06-02-2025/estructura-if.js
@@ -7,11 +7,43 @@ estructura if en javascript
 */
 
 
-let edad =18 
-let tieneIne= true 
+let edad =10 
+let tieneIne= false
 
 
-
+// Condicional simple
 if (edad >= 18 && tieneIne) {
     console.log("Tienes los requisitos para votar")
 }
+
+//Condicional con else
+if (edad >= 18 && tieneIne) {
+    console.log("Tienes los requisitos para votar")
+}else{
+    console.log("No tienes los requisitos para votar")
+}
+
+console.log("Fin del programa")
+
+
+//Condicional con else if
+if (edad < 13) {
+    console.log("Eres un niño")
+}else if (edad < 18){
+    console.log("Eres un adolescente")
+} else {
+    console.log("Eres un adulto")
+}
+
+console.log("Fin del programa")
+
+
+let tieneEntrada = false 
+
+if (!tieneEntrada) {
+    console.log("Debes cmomprar una entrada")
+} else {
+    console.log("Puedes entrar")
+}
+
+console.log("Fin del programa")
