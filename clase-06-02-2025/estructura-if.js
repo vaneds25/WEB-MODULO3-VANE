d331b6eb157399801f7e8f2711c25f1fc7360b9d
Vane <vaneduransierra@gmail.com>
2025-03-02 16:28:38 -0600

    estructura-js

---
+++ clase-06-02-2025/estructura-if.js
@@ -0,0 +1,17 @@
+/*
+
+estructura if en javascript
+-Permite ejecutar código solo si se cumple una condición 
+- Puede usarse con operadores de comparación y lógicos
+
+*/
+
+
+let edad =18 
+let tieneIne= true 
+
+
+
+if (edad >= 18 && tieneIne) {
+    console.log("Tienes los requisitos para votar")
+}
--- clase-06-02-2025/operadores.js
+++ clase-06-02-2025/operadores.js
@@ -59,3 +59,38 @@ let mayorIgual = 10 >= 5 //true
 let menorIgual = 10 <= 5 //false
 
 console.log(mayor, menor, igual, igual2, estrictamenteigual, diferente, estrictamenteDiferente, mayorIgual, menorIgual)
+
+//Operadores Lógicos
+// and funciona como un && y sirve para ambos
+let and = true && true //true
+let and2 = (10 > 5) && (3 < 8) //true
+// or funciona como un || y sirve para una o la otra
+let or = true || false //true es una o la otra
+let or2 = (10 > 5) || (3 < 8) //true
+//not funciona como un ! y sirve para negar
+let not = !(10 > 5) //false porque lo estoy negando 
+
+console.log(and, and2, or, or2, not)
+
+/*
+Jerarquia de Operadores Lógicos
+1. NOT ! -> se evalúa primero 
+2. AND && -> se evalúa segundo 
+3. OR || -> se evalúa tercero
+
+Nora: Siempre se evalúa lo que haya entre paréntesis primero 
+
+*/
+
+//Ejemplo de jerarquía de operadores lógicos
+var logico1 = true || false && false // Primero evaluó AND = TRUE || FALSE = TRUE
+var logico2 = (TRUE || FALSE) && FALSE // (TRUE) && FALSE = FALSE
+var logico3 = true && true || false
+
+console.log(logico1, logico2, logico3)
+
+
+
+
+
+
