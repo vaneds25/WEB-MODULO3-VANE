7d19353ef0a4da92bae4ac8a1b3a799a09211c77
Vane <vaneduransierra@gmail.com>
2025-03-01 23:19:00 -0600

    first commit

---
+++ P1-intro-javascript/tipos-de-dato.js
@@ -0,0 +1,16 @@
+console.log(typeof 42);            // number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666);          // number
+console.log(typeof true);          // boolean
+console.log(typeof 0);             // number
+console.log(typeof '');            // string
+console.log(typeof null);          // object (caso especial en JS)
+console.log(typeof undefined);     // undefined
+console.log(typeof FALSE);         // boolean
+
+// Ejemplos adicionales
+console.log(typeof []);            // object (arreglo)
+console.log(typeof {});            // object (objeto)
+console.log(typeof function(){});  // function
+console.log(typeof NaN);           // number (aunque es "Not-a-Number")
+console.log(typeof Infinity);      // number
